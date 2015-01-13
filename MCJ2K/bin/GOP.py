3a18c3c549a82a42ea8019d947fdac1fbe1b6808
josejuansanchez <josejuan.sanchez@gmail.com>
2015-01-13 11:18:10 +0100

    Changes in Makefiles and files deleted

--- MCJ2K/bin/GOP.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: iso-8859-15 -*-
-
-# Calcula el tamaño del GOP en función del número de niveles
-# temporales.
-
-class GOP:
-
-    def get_size(self, temporal_levels):
-        return 2**(temporal_levels - 1)
