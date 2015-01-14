4ec41fd8ecde4b9b351ee7f55c2478b93fcd11ec
josejuansanchez <josejuan.sanchez@gmail.com>
2015-01-14 14:02:40 +0100

    Little changes with knapsack tools

--- doc/md/compile.md
+++ doc/md/compile.md
@@ -1,15 +1,29 @@
 # Compiling instructions
 
+## Step 0. Add images and Kakadu source code:
+
+* Add the folder with the images in: 
+
+```
+cr_mc_j2k/data
+```
+
+* Add KaKadu software in:
+
+```
+cr_mc_j2k/src/Kakadu
+```
+
 ## Step 1. Compile Kakadu
 
-Example:
+Example on Linux:
 
 ```
-~/cr_mc_j2k/src/Kakadu/v6_4_1-00305L/make/make -f Makefile-Linux-x86-64-gcc
+cr_mc_j2k/src/Kakadu/v6_4_1-00305L/make/make -f Makefile-Linux-x86-64-gcc
 ```
 
-## Step 2. Compile CR tools
+## Step 2. Compile all the tools
 
 ```
-/home/josejuan/cr_mc_j2k/src/CR/make
+cr_mc_j2k/make/make
 ```
--- knapsack/info_files/create_all_info_files.sh
+++ knapsack/info_files/create_all_info_files.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-#set -x
+set -x
+
+CONFIGFILE="../../scripts/cr_mc_config.cfg"
+if [ ! -f $CONFIGFILE ]; then
+	echo "Error reading config file: $CONFIGFILE"
+	exit;
+fi
+
+echo "Reading config file: $CONFIGFILE ...."
+source "$CONFIGFILE"
 
 if [ $# -ne 2 ]; then
 	echo "Uso: $0 <numero_inicio_secuencia> <numero_fin_secuencia>"
@@ -9,19 +18,6 @@ fi
 
 START=$1
 END=$2
-SOURCE_IMAGE_J2C_FILES="/home/josejuan/experimentos/experimentos_cr/data/thumbnails"
-
-# Tools
-CREATE_WOI_LIST=/home/josejuan/experimentos/experimentos_cr/conditional_replenishment/knapsack/j2c_to_knapsack_files/create_woi_list
-CREATE_KNAPSACK_JSON_FILE=/home/josejuan/experimentos/experimentos_cr/conditional_replenishment/knapsack/j2c_to_knapsack_files/create_knapsack_json_file.sh
-
-# Dimensiones de la imagen original
-WIDTH_RECONS=1280
-HEIGHT_RECONS=768
-
-# Dimensiones de los precintos de la imagen original
-W_PRECINT_SIZE=128
-H_PRECINT_SIZE=128
 
 # Archivo que contiene toda la lista de WOIs
 WOIS_LIST_FILE=wois.txt
@@ -33,7 +29,7 @@ i=$START
 while [ $i -le $END ]; do
 	i_index=`printf %03d $i`
 
-	$CREATE_KNAPSACK_JSON_FILE $SOURCE_IMAGE_J2C_FILES/${i_index}.j2c $WOIS_LIST_FILE > files/${i_index}.json
+	$CREATE_KNAPSACK_JSON_FILE $THUMBNAILS_DIRECTORY/${i_index}.j2c $WOIS_LIST_FILE > files/${i_index}.json
 
 	i=$(($i+1))
 done
