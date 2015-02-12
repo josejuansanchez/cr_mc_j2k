197a67bb20c909dfa5c03b5858f7aaf0d05ac3b4
josejuansanchez <josejuan.sanchez@gmail.com>
2015-02-12 14:06:01 +0100

    New script has been added for viewing the motion vectors obtained with the 'me' tool

---
+++ scripts/gpt/view_me_vectors.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+IMG_OUT=$1
+DATA=$1
+
+(echo "set term svg";
+echo "set output '${IMG_OUT}.svg'";
+echo "set nokey";
+echo "plot '$DATA' using 1:2:3:4  with vec";
+echo "exit") | gnuplot
+
+display ${IMG_OUT}.svg
+
+
