42908eea3ab05571e45d014120bac2939df3f603
josejuansanchez <josejuan.sanchez@gmail.com>
2015-02-27 13:06:02 +0100

    Updated the script parameters

--- scripts/evaluate_bitrates.sh
+++ scripts/evaluate_bitrates.sh
@@ -13,21 +13,21 @@ if [[ $CR_MC_J2K_HOME = "" ]]; then
     exit 1
 fi
 
-EXPERIMENTS=$CR_MC_J2K_HOME/experiments/2015/2015_02_25_stockholm_layers-8_levels-2_precincts-128-64-32_blk-32_with_me_a_0_mode_auto
+EXPERIMENTS=$CR_MC_J2K_HOME/experiments/2015/2015_02_27_speedway_layers-8_levels-2_precincts_64-32-16_blk-16_no_me_a0_v2_mode_2_8
 
-# tree
-#IMAGE_N=0
-#IMAGE_N1=1
+# speedway
+IMAGE_N=0
+IMAGE_N1=1
 
 # stockholm
-IMAGE_N=4
-IMAGE_N1=5
+#IMAGE_N=4
+#IMAGE_N1=5
 
 # big buck bunny
 #IMAGE_N=100
 #IMAGE_N1=101
 
-for((BITRATE=61000; BITRATE<=81000; BITRATE=BITRATE+10000))
+for((BITRATE=1000; BITRATE<=5000; BITRATE=BITRATE+1000))
 #for((BITRATE=11000; BITRATE<=51000; BITRATE=BITRATE+10000))
 do
     ./sequential.sh $IMAGE_N $IMAGE_N1 $BITRATE
@@ -42,7 +42,7 @@ do
 
     # TODO: Temporal solution - 0 and 90 are values hardcoded
     #./create_ogv_and_gif.sh 0 90
-    ./create_ogv_and_gif.sh 6 10
+    ./create_ogv_and_gif.sh 0 50
 
     mv all.ogv all_$BITRATE.ogv
     mv all.gif all_$BITRATE.gif
