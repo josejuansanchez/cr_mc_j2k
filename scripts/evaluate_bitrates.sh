667915db7d08bd60030277e41b5c060ff777e6e4
josejuansanchez <josejuan.sanchez@gmail.com>
2015-03-09 13:40:51 +0100

    Changes for a new experiment with another progression

--- scripts/evaluate_bitrates.sh
+++ scripts/evaluate_bitrates.sh
@@ -13,7 +13,7 @@ if [[ $CR_MC_J2K_HOME = "" ]]; then
     exit 1
 fi
 
-EXPERIMENTS=$CR_MC_J2K_HOME/experiments/2015/2015_03_09_speedway_layers-8_levels-2_precincts_64-32-16_blk-16_no_me_mode_2_rpcl
+EXPERIMENTS=$CR_MC_J2K_HOME/experiments/2015/2015_03_09_speedway_layers-8_levels-2_precincts_64-32-16_blk-16_no_me_mode_2_rlcp
 
 # speedway
 IMAGE_N=0
