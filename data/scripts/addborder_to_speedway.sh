bbd77a4b94585219c33564a8b64caabe00d6830a
josejuansanchez <josejuan.sanchez@gmail.com>
2015-06-01 13:04:26 +0200

    Some variables have been renamed

--- data/scripts/addborder_to_speedway.sh
+++ data/scripts/addborder_to_speedway.sh
@@ -10,18 +10,18 @@ if [ $# -ne 2 ]; then
 	exit -1
 fi
 
-INICIO=$1
-FIN=$2
-IMAGES_ORIGINALES_PGM="$CR_MC_J2K_HOME/data/sequences/speedway_352x288"
-IMAGES_DESTINO_PGM="$CR_MC_J2K_HOME/data/sequences/speedway_384x320"
+INITIAL=$1
+FINAL=$2
+IMAGES_PGM_SOURCE="$CR_MC_J2K_HOME/data/sequences/speedway_352x288"
+IMAGES_PGM_DESTINATION="$CR_MC_J2K_HOME/data/sequences/speedway_384x320"
 
-i=$INICIO
-while [ $i -le $FIN ]; do
+i=$INITIAL
+while [ $i -le $FINAL ]; do
 
   i_index=`printf %03d $i`
 
   echo "Processing image: $i_index"
-  convert -bordercolor black -border 16x16 $IMAGES_ORIGINALES_PGM/${i_index}.pgm $IMAGES_DESTINO_PGM/${i_index}.pgm
+  convert -bordercolor black -border 16x16 $IMAGES_PGM_SOURCE/${i_index}.pgm $IMAGES_PGM_DESTINATION/${i_index}.pgm
   
   i=$(($i+1))
 
