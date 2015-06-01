7e2b2db30ac72b10411990c99727b7f7dc14c815
josejuansanchez <josejuan.sanchez@gmail.com>
2015-06-01 13:17:36 +0200

    Updated references to the 'thumbnails' directory

--- data/scripts/clean_pgm_j2c.sh
+++ data/scripts/clean_pgm_j2c.sh
@@ -5,11 +5,11 @@ if [[ $CR_MC_J2K_HOME = "" ]]; then
 	exit 1
 fi
 
-rm $CR_MC_J2K_HOME/data/thumbnails/*.j2c
-rm $CR_MC_J2K_HOME/data/thumbnails/*.pgm
+rm $CR_MC_J2K_HOME/data/j2c_and_thumbnails/*.j2c
+rm $CR_MC_J2K_HOME/data/j2c_and_thumbnails/*.pgm
 
-rm $CR_MC_J2K_HOME/data/thumbnails/*.cache
-rm $CR_MC_J2K_HOME/data/thumbnails/*.lrcp
-rm $CR_MC_J2K_HOME/data/thumbnails/*.woi
-rm $CR_MC_J2K_HOME/data/thumbnails/*.sort
-rm $CR_MC_J2K_HOME/data/thumbnails/*.ord
+rm $CR_MC_J2K_HOME/data/j2c_and_thumbnails/*.cache
+rm $CR_MC_J2K_HOME/data/j2c_and_thumbnails/*.lrcp
+rm $CR_MC_J2K_HOME/data/j2c_and_thumbnails/*.woi
+rm $CR_MC_J2K_HOME/data/j2c_and_thumbnails/*.sort
+rm $CR_MC_J2K_HOME/data/j2c_and_thumbnails/*.ord
