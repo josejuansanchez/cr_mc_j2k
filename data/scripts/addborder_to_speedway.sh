9d6e08f88818804b012c72874dfceaaf4f58d1d2
josejuansanchez <josejuan.sanchez@gmail.com>
2015-07-08 13:33:47 +0200

    Minor changes to include the border size as input parameter

--- data/scripts/addborder_to_speedway.sh
+++ data/scripts/addborder_to_speedway.sh
@@ -5,15 +5,16 @@ if [[ $CR_MC_J2K_HOME = "" ]]; then
 	exit 1
 fi
 
-if [ $# -ne 2 ]; then
-	echo "Uso: $0 <initial frame #> <final frame #>"
+if [ $# -ne 3 ]; then
+	echo "Uso: $0 <initial frame #> <final frame #> <border-width x border-weight>"
 	exit -1
 fi
 
 INITIAL=$1
 FINAL=$2
+BORDER=$3
 IMAGES_PGM_SOURCE="$CR_MC_J2K_HOME/data/sequences/speedway_352x288"
-IMAGES_PGM_DESTINATION="$CR_MC_J2K_HOME/data/sequences/speedway_384x320"
+IMAGES_PGM_DESTINATION="$CR_MC_J2K_HOME/data/sequences/speedway_512x512"
 
 i=$INITIAL
 while [ $i -le $FINAL ]; do
@@ -21,7 +22,7 @@ while [ $i -le $FINAL ]; do
   i_index=`printf %03d $i`
 
   echo "Processing image: $i_index"
-  convert -bordercolor black -border 16x16 $IMAGES_PGM_SOURCE/${i_index}.pgm $IMAGES_PGM_DESTINATION/${i_index}.pgm
+  convert -bordercolor black -border $BORDER $IMAGES_PGM_SOURCE/${i_index}.pgm $IMAGES_PGM_DESTINATION/${i_index}.pgm
   
   i=$(($i+1))
 
