c25b370348bbbe58d7fbb84681e0679f263bcff1
José Juan Sánchez <josejuan.sanchez@gmail.com>
2020-02-29 11:10:35 +0100

    Added bash script

---
+++ set_env_vars.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=/vagrant/src/Kakadu/v6_4_1-00305L/lib/Linux-x86-64-gcc
+export PATH=$PATH:/vagrant/src/Kakadu/v6_4_1-00305L/bin/Linux-x86-64-gcc
+export CR_MC_J2K_HOME=/vagrant
